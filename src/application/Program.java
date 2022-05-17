5e848de603f53ad2dc55064b3f7f40c85f04a072
unknown <wr.wallacerocha@gmail.com>
2022-05-17 13:42:39 -0300

    Classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,22 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 		Departamento dp = new Departamento(1, "Books");
-		
-		System.out.println(dp);
-		
+		Vendedor vend = new Vendedor(2, "Maria", "maria@gmail.com", new Date(), 5000.0, dp);
+
+		System.out.println(vend);
+
 	}
 
 }
