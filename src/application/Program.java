b8711743a4edbcf47310fc364a9049bbfffce981
unknown <wr.wallacerocha@gmail.com>
2022-05-17 13:27:18 -0300

    Classe Departamento

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Departamento dp = new Departamento(1, "Books");
+		
+		System.out.println(dp);
+		
+	}
+
+}
